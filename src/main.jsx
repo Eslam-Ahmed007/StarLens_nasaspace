c253571f1e25a2fcd256b8babdac24062224506f
Eslam-Ahmed007 <eslam73948@gmail.com>
2024-10-05 06:49:42 +0300

    upload

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,8 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/StarLens_nasaspace/" >
+    {/* <BrowserRouter basename="/StarLens_nasaspace/" > */}
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>,
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base : "/StarLens_nasaspace/" ,
+  // base : "/StarLens_nasaspace/" ,
+  build: {
+    outDir: 'dist', // Output folder for production build
+    assetsDir: 'assets', // Where to put assets like images
+  },
 })
